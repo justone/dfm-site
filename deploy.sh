50489eb47e14212d0146c237c37f5ae4f8865daa
Nate Jones <nate@endot.org>
2015-05-14 23:24:02 -0400

    handle deploys for branches

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-rsync -avze 'ssh -p 22' site/ endot.org@endot.org:~/domains/dfm.endot.org/html/
+set -e
+SITE=dfm.endot.org
+
+if [[ $DRONE_BRANCH == "master" ]]; then
+    rsync -aze 'ssh -p 22' site/ endot.org@endot.org:/home/54/domains/$SITE/html/
+else
+    ssh endot.org@endot.org mkdir -p /home/54/domains/${DRONE_BRANCH}-$SITE/html/
+    rsync -aze 'ssh -p 22' site/ endot.org@endot.org:/home/54/domains/${DRONE_BRANCH}-$SITE/html/
+fi
